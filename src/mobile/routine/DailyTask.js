37ab6fb20fa6761cd2c7fad1aede0f438e1287d5
wasp0094 <90551491+wasp0094@users.noreply.github.com>
2022-02-26 16:00:31 +0530

    update daily task

--- src/mobile/routine/DailyTask.js
+++ src/mobile/routine/DailyTask.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import BottomNav from '../BottomNav';
+import '../Explore.css';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { IconButton } from '@mui/material';
 
 function DailyTask() {
     return (
@@ -7,8 +15,23 @@ function DailyTask() {
             <div>
                 <h1>daily tasks</h1>
             </div>
-
-            <BottomNav />
+            <Card sx={{ minWidth: 275, background: '#d7e3fc' }}>
+                <CardContent sx={{ padding: 1 }}>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Exercise tag
+                    </Typography>
+                    <Typography variant="h6" component="div">
+                        Exercise Name/Title
+                    </Typography>
+                    {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        adjective
+                    </Typography> */}
+                </CardContent>
+                <CardActions sx={{ pt: 0 }}>
+                    <Button size="small">Start Now</Button>
+                    <IconButton><CheckCircleOutlineIcon fontSize='small' color="action" /></IconButton>
+                </CardActions>
+            </Card>
         </>
     )
 }
--- src/mobile/routine/Dashboard.js
+++ src/mobile/routine/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BottomNav from '../BottomNav';
+//import BottomNav from '../BottomNav';
 
 function Dashboard() {
     return (
@@ -7,7 +7,7 @@ function Dashboard() {
             <div>
                 <h1>Dashboard</h1>
             </div>
-            <BottomNav />
+            {/* <BottomNav /> */}
         </>
     )
 }
