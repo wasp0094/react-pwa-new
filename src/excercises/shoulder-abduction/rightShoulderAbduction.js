056031d795d03ea2caadfca2b0119cb58ca17490
Tia Saxena <tia.saxena041202@gmail.com>
2022-06-13 14:26:40 +0530

    left abduction fixes

--- src/excercises/shoulder-abduction/rightShoulderAbduction.js
+++ src/excercises/shoulder-abduction/rightShoulderAbduction.js
@@ -3,11 +3,11 @@ let setsCompleted = 0;
 let up = false,
   down = false;
 let maxAngle = 10;
-let maxAngleSum = 0;
 let dayRange = 0;
-let calibrationAngle = 0;
+let maxAngleSum = 0;
+let calibrationAngle = 0, finalCalibrationAngle = 0;
 let t0, t1;
-let tc0, tc1, calibratedTime; // tc --> time_calibration stamp
+let tc0, tc1, calibratedTime = 0, finalCalibrationTime = 0; // tc --> time_calibration stamp
 let flag = 0;
 
 function speak_js(message) {
@@ -22,42 +22,40 @@ export default function rightShoulderAbduction(
 ) {
   const { requiredReps } = excerciseVars;
   const obj11 = points[11];
-  const obj13 = points[13];
+  const obj15 = points[15];
   const vector1 = [obj11.x - obj11.x, obj11.y - obj11.y - 0.3];
-  const vector2 = [obj11.x - obj13.x, obj11.y - obj13.y];
+  const vector2 = [obj11.x - obj15.x, obj11.y - obj15.y];
+
   const dot = vector1[0] * vector2[0] + vector1[1] * vector2[1];
   const mod_a = Math.sqrt(vector1[0] * vector1[0] + vector1[1] * vector1[1]);
   const mod_b = Math.sqrt(vector2[0] * vector2[0] + vector2[1] * vector2[1]);
+
   const angle = ((Math.acos(dot / (mod_a * mod_b)) * 180) / 3.14).toFixed(2);
   maxAngle = Math.max(maxAngle, angle);
 
   if (flag === 0) {
     speak_js(
-      "Stretch your arms to the maximum possible as this set helps us to calibrate"
+      "StretchYourArmsToTheMaximumPossibleAsThisSetHelpsUsToCalibrate"
     );
     flag = 1;
   }
 
+  if(setsCompleted === 0) {
+    finalCalibrationAngle = 30;
+  } else {
+    finalCalibrationAngle = (calibrationAngle / requiredReps).toFixed(2);
+  }
+
   if (angle <= 30) {
+
     down = true;
     if (up === false && setsCompleted === 0)
-      tc0 = new Date().getSeconds() + new Date().getMinutes() * 60;
+      tc0 = Number(new Date().getSeconds() + new Date().getMinutes() * 60);
     if (up === false)
-      t0 = new Date().getSeconds() + new Date().getMinutes() * 60;
-  } else if (
-    angle >= (setsCompleted === 0 ? 40 : calibrationAngle / requiredReps)
-  ) {
+      t0 = Number(new Date().getSeconds() + new Date().getMinutes() * 60);
+      
+  } else if (angle >= Number(angle) >= Number(finalCalibrationAngle)) {
     up = true;
-    console.log(
-      "calibrated angle = ",
-      (setsCompleted === 0 ? 40 : calibrationAngle / requiredReps) +
-        " angle " +
-        angle
-    );
-    // console.log(
-    //   "The calibrated time is = ",
-    //   calibratedTime + " setsCompleted " + setsCompleted
-    // );
   }
   if (up === true && down === true) {
     repsCompleted += 1;
@@ -65,42 +63,51 @@ export default function rightShoulderAbduction(
     down = false;
 
     if (repsCompleted % 2 === 0) {
+
+      
+      if (setsCompleted === 0) {
+        tc1 = Number(new Date().getSeconds() + new Date().getMinutes() * 60);
+        calibratedTime = Number(calibratedTime + tc1 - tc0);
+      }
+      t1 = Number(new Date().getSeconds() + new Date().getMinutes() * 60);
+      
+      if(setsCompleted === 0) {
+        finalCalibrationTime = 30;
+      } else {
+        finalCalibrationTime = (calibratedTime / requiredReps) + 5;
+      }
+
+      const reps = (repsCompleted / 2).toString();
+      const sets = setsCompleted.toString();
+
+      if(repsCompleted === 0) {
+        speak_js(
+          sets +
+            "sets"
+        );
+      }
+
       speak_js(
-        (repsCompleted / 2).toString() +
-          "reps" +
-          setsCompleted.toString() +
-          "sets"
+        reps +
+          "reps"
       );
 
-      if (setsCompleted === 0) {
-        tc1 = new Date().getSeconds() + new Date().getMinutes() * 60;
-        calibratedTime += tc1 - tc0;
+      if((setsCompleted !== 0) && ((t1 - t0) > finalCalibrationTime)) {
+        speak_js("TooSlow");
       }
-      t1 = new Date().getSeconds() + new Date().getMinutes() * 60;
-      // console.log(t0 + " " + t1);
-      // if (
-      //   setsCompleted !== 0 &&
-      //   (t1 - t0 > (setsCompleted === 0)
-      //     ? 30
-      //     : calibratedTime / requiredReps + 6)
-      // ) {
-      //   speak_js("Too slow");
-      // }
-      if (
-        setsCompleted !== 0 &&
-        t1 - t0 > (setsCompleted === 0 ? 30 : calibratedTime / requiredReps + 6)
-      ) {
-        speak_js("Too slow");
-      }
+
       window.t0 = t1;
 
       maxAngleSum += maxAngle;
-      dayRange = (
-        maxAngleSum /
-        (repsCompleted / 2 + setsCompleted * requiredReps)
-      ).toFixed(2);
-      calibrationAngle += setsCompleted === 0 ? maxAngle : 0;
-      maxAngle = 10;
+       dayRange = (maxAngleSum / (repsCompleted / 2 + (setsCompleted * requiredReps))).toFixed(2);
+
+       if(dayRange && setsCompleted === 0) {
+        calibrationAngle = Number(calibrationAngle) + Number(dayRange);
+      } else {
+        calibrationAngle += 0;
+      }
+      
+      maxAngle = 30;
       setExcerciseVars({
         ...excerciseVars,
         repsCompleted: Math.ceil(repsCompleted / 2),
